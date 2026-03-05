cf0d352107bb514cf930e8c6add86983c42e9af2
Bruno Manoel Toebe de Almeida <bruno.233397@edu.unipar.br>
2026-03-05 00:37:39 -0300

    .

--- src/main/java/com/legiaotricolor/BackLegiaoTricolor/SecurityConfig.java
+++ src/main/java/com/legiaotricolor/BackLegiaoTricolor/SecurityConfig.java
@@ -3,36 +3,24 @@ package com.legiaotricolor.BackLegiaoTricolor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    private final JwtAuthenticationFilter jwtFilter;
-
-    public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(cors -> {}) // 👈 nova forma correta
+                .cors(cors -> {}) // 👈 ISSO ATIVA O CorsConfig
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .requestMatchers("/users/**").permitAll()
+                        .requestMatchers("/admin/**").permitAll()
                         .anyRequest().permitAll()
                 );
 
