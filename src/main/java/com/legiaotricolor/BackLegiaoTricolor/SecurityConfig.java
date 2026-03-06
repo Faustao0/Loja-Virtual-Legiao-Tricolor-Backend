9300e4f13b09b4d04a54cfeb2b65d6a67a938880
Bruno Manoel Toebe de Almeida <bruno.233397@edu.unipar.br>
2026-03-05 23:45:36 -0300

    .

--- src/main/java/com/legiaotricolor/BackLegiaoTricolor/SecurityConfig.java
+++ src/main/java/com/legiaotricolor/BackLegiaoTricolor/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.legiaotricolor.BackLegiaoTricolor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,9 +13,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(cors -> {}) // 👈 ISSO ATIVA O CorsConfig
+                .cors(cors -> {}) // ativa o CorsConfig
                 .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 👈 LIBERA PREFLIGHT
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
