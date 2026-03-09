93a0e227458f64f6a3e9764e5432b3c363be84da
Bruno Manoel Toebe de Almeida <bruno.233397@edu.unipar.br>
2026-03-09 00:28:42 -0300

    .

--- src/main/java/com/legiaotricolor/BackLegiaoTricolor/CategoryInitializer.java
+++ src/main/java/com/legiaotricolor/BackLegiaoTricolor/CategoryInitializer.java
@@ -2,15 +2,17 @@ package com.legiaotricolor.BackLegiaoTricolor;
 
 import com.legiaotricolor.BackLegiaoTricolor.domain.Category;
 import com.legiaotricolor.BackLegiaoTricolor.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class CategoryInitializer implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
+
+    public CategoryInitializer(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public void run(String... args) {
@@ -25,10 +27,9 @@ public class CategoryInitializer implements CommandLineRunner {
 
         if (categoryRepository.findByNameIgnoreCase(name).isEmpty()) {
 
-            Category category = Category.builder()
-                    .name(name)
-                    .description(description)
-                    .build();
+            Category category = new Category();
+            category.setName(name);
+            category.setDescription(description);
 
             categoryRepository.save(category);
         }
