d463f67c601006306c154250103536e7949705bd
Bruno Manoel Toebe de Almeida <bruno.233397@edu.unipar.br>
2026-03-14 22:28:02 -0300

    .

--- src/main/java/com/legiaotricolor/BackLegiaoTricolor/CorsConfig.java
+++ src/main/java/com/legiaotricolor/BackLegiaoTricolor/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://faustao0.github.io"
         ));
 
         configuration.setAllowedMethods(List.of(
